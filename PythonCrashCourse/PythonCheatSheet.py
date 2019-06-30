14a5b86c96f1abcfb3744314a4e15956b71b0d1f
prasad <417.prasad@gmail.com>
2019-06-30 17:13:29 -0500

    Numpy cheat sheet

--- PythonCrashCourse/PythonCheatSheet.py
+++ PythonCrashCourse/PythonCheatSheet.py
@@ -1,19 +1,5 @@
 import keyword
-from builtins import print
-
-
-class Color:
-    PURPLE = '\033[95m'
-    CYAN = '\033[96m'
-    DARKCYAN = '\033[36m'
-    BLUE = '\033[94m'
-    GREEN = '\033[92m'
-    YELLOW = '\033[93m'
-    RED = '\033[91m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-    END = '\033[0m'
-
+from utils.Color import Color
 
 print(f'{Color.BOLD}Hello! Welcome to python course.{Color.END}')
 print('                                                                         ')
