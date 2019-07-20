f6d67b9de86f0c4f990319b204a93cdfef236712
prasad <417.prasad@gmail.com>
2019-07-20 02:52:48 -0500

    Bank app tutorial

--- imdb/imdb.sql
+++ imdb/imdb.sql
@@ -1,107 +1,106 @@
 -- SHOW VARIABLES LIKE "secure_file_priv";
 
 -- SELECT @@GLOBAL.secure_file_priv;
-DROP DATABASE imdb;
+-- DROP DATABASE imdb;
 
-CREATE DATABASE imdb;
+-- CREATE DATABASE imdb;
 
-USE imdb;
+-- USE imdb;
 
-CREATE TABLE `name.basics` (
-  `nconst` text,
-  `primaryName` text,
-  `birthYear` text,
-  `deathYear` text,
-  `primaryProfession` text,
-  `knownForTitles` text
-);
+-- CREATE TABLE `name.basics` (
+--   `nconst` text,
+--   `primaryName` text,
+--   `birthYear` text,
+--   `deathYear` text,
+--   `primaryProfession` text,
+--   `knownForTitles` text
+-- );
 
-CREATE TABLE `title.akas` (
-  `titleid` text,
-  `ordering` text,
-  `title` text,
-  `region` text,
-  `languages` text,
-  `types` text,
-  `attributes` text,
-  `isOriginalTitle` text
-);
+-- CREATE TABLE `title.akas` (
+--   `titleid` text,
+--   `ordering` text,
+--   `title` text,
+--   `region` text,
+--   `languages` text,
+--   `types` text,
+--   `attributes` text,
+--   `isOriginalTitle` text
+-- );
 
-CREATE TABLE `title.basics` (
-  `tconst` text,
-  `titleType` text,
-  `primaryTitle` text,
-  `originalTitle` text,
-  `isAdult` text,
-  `startYear` text,
-  `endYear` text,
-  `runtimeMinutes` text,
-  `genres` text
-);
-	
-CREATE TABLE `title.crew` (
-  `tconst` text,
-  `directors` text,
-  `writers` text
-);
+-- CREATE TABLE `title.basics` (
+--   `tconst` text,
+--   `titleType` text,
+--   `primaryTitle` text,
+--   `originalTitle` text,
+--   `isAdult` text,
+--   `startYear` text,
+--   `endYear` text,
+--   `runtimeMinutes` text,
+--   `genres` text
+-- );
+-- 	
+-- CREATE TABLE `title.crew` (
+--   `tconst` text,
+--   `directors` text,
+--   `writers` text
+-- );
 
-CREATE TABLE `title.principals` (
-  `tconst` text,
-  `ordering` text,
-  `nconst` text,
-  `category` text,
-  `job` text,
-  `characters` text
-);
+-- CREATE TABLE `title.principals` (
+--   `tconst` text,
+--   `ordering` text,
+--   `nconst` text,
+--   `category` text,
+--   `job` text,
+--   `characters` text
+-- );
 
-CREATE TABLE `title.ratings` (
-	`tconst` text,
-	`averageRating` text,
-	`numVotes` text
-);
+-- CREATE TABLE `title.ratings` (
+-- 	`tconst` text,
+-- 	`averageRating` text,
+-- 	`numVotes` text
+-- );
 
-LOAD DATA INFILE '/Users/prasaddy/mysql/name.basics.csv' 
-INTO TABLE `name.basics`
-FIELDS TERMINATED BY '\t' 
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS;
-
-LOAD DATA INFILE '/Users/prasaddy/mysql/title.akas.csv' 
-INTO TABLE `title.akas`
-FIELDS TERMINATED BY '\t' 
-LINES TERMINATED BY '\n';
+-- LOAD DATA INFILE '/Users/prasaddy/mysql/name.basics.csv' 
+-- INTO TABLE `name.basics`
+-- FIELDS TERMINATED BY '\t' 
+-- LINES TERMINATED BY '\n'
 -- IGNORE 1 ROWS;
 
-LOAD DATA INFILE '/Users/prasaddy/mysql/title.basics.csv' 
-INTO TABLE `title.basics`
-FIELDS TERMINATED BY '\t' 
-LINES TERMINATED BY '\n';
--- IGNORE 1 ROWS;
+-- LOAD DATA INFILE '/Users/prasaddy/mysql/title.akas.csv' 
+-- INTO TABLE `title.akas`
+-- FIELDS TERMINATED BY '\t' 
+-- LINES TERMINATED BY '\n';
+-- -- IGNORE 1 ROWS;
 
-LOAD DATA INFILE '/Users/prasaddy/mysql/title.crew.csv' 
-INTO TABLE `title.crew`
-FIELDS TERMINATED BY '\t' 
-LINES TERMINATED BY '\n';
--- IGNORE 1 ROWS;
+-- LOAD DATA INFILE '/Users/prasaddy/mysql/title.basics.csv' 
+-- INTO TABLE `title.basics`
+-- FIELDS TERMINATED BY '\t' 
+-- LINES TERMINATED BY '\n';
+-- -- IGNORE 1 ROWS;
 
-LOAD DATA INFILE '/Users/prasaddy/mysql/title.ratings.csv' 
-INTO TABLE `title.ratings`
-FIELDS TERMINATED BY '\t' 
-LINES TERMINATED BY '\n';
--- IGNORE 1 ROWS;
+-- LOAD DATA INFILE '/Users/prasaddy/mysql/title.crew.csv' 
+-- INTO TABLE `title.crew`
+-- FIELDS TERMINATED BY '\t' 
+-- LINES TERMINATED BY '\n';
+-- -- IGNORE 1 ROWS;
 
-LOAD DATA INFILE '/Users/prasaddy/mysql/title.principals.csv' 
-INTO TABLE `title.principals`
-FIELDS TERMINATED BY '\t' 
-LINES TERMINATED BY '\n';
--- IGNORE 1 ROWS;
+-- LOAD DATA INFILE '/Users/prasaddy/mysql/title.ratings.csv' 
+-- INTO TABLE `title.ratings`
+-- FIELDS TERMINATED BY '\t' 
+-- LINES TERMINATED BY '\n';
+-- -- IGNORE 1 ROWS;
 
+-- LOAD DATA INFILE '/Users/prasaddy/mysql/title.principals.csv' 
+-- INTO TABLE `title.principals`
+-- FIELDS TERMINATED BY '\t' 
+-- LINES TERMINATED BY '\n';
+-- -- IGNORE 1 ROWS;
 
-SELECT COUNT(nconst) FROM `name.basics`;
-SELECT COUNT(titleid) FROM `title.akas`;
-SELECT COUNT(tconst) FROM `title.basics`;
-SELECT COUNT(tconst) FROM `title.crew`;
-SELECT COUNT(tconst) FROM `title.principals`;
+-- SELECT COUNT(nconst) FROM `name.basics`;
+-- SELECT COUNT(titleid) FROM `title.akas`;
+-- SELECT COUNT(tconst) FROM `title.basics`;
+-- SELECT COUNT(tconst) FROM `title.crew`;
+-- SELECT COUNT(tconst) FROM `title.principals`;
 
 SELECT * FROM imdb.`name.basics` 
 WHERE primaryName IN ('Taraka Rama Rao Nandamuri', 'Akkineni Nageshwara Rao', 'S.V. Ranga Rao', 'Krishna Ghattamanen', 'Venkatesh Daggubati', 'Chiranjeevi', 'Mahesh Babu', 'Pawan Kalyan', 'N.T. Rama Rao Jr.', 'Nandamuri Balakrishna', 'Allu Arjun', 'Nagarjuna Akkineni',
