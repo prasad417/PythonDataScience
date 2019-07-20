6037b0c71165c6ecb57e04b95ea674a8e1463ae8
prasad <417.prasad@gmail.com>
2019-07-20 11:24:06 -0500

    SQL query update

--- imdb/imdb.sql
+++ imdb/imdb.sql
@@ -63,8 +63,8 @@
 -- LOAD DATA INFILE '/Users/prasaddy/mysql/name.basics.csv' 
 -- INTO TABLE `name.basics`
 -- FIELDS TERMINATED BY '\t' 
--- LINES TERMINATED BY '\n'
--- IGNORE 1 ROWS;
+-- LINES TERMINATED BY '\n';
+-- -- IGNORE 1 ROWS;
 
 -- LOAD DATA INFILE '/Users/prasaddy/mysql/title.akas.csv' 
 -- INTO TABLE `title.akas`
@@ -108,7 +108,45 @@ WHERE primaryName IN ('Taraka Rama Rao Nandamuri', 'Akkineni Nageshwara Rao', 'S
  'Tottempudi Gopichand', 'Ram Pothineni', 'Ram Pothineni', 'Varun Tej', 'Sai Dharam Tej', ' Sumanth', 'Akhil Akkineni', 'Vishnu Manchu', 'Manoj Kumar Manchu',
  'Sai Srinivas Bellamkonda', 'Nara Rohith', 'Mohan Babu', 'Naga Shaurya', 'Sundeep Kishan', 'Varun Sandesh', 'Kalyan Ram', 'Aadi', 'Rajendra Prasad', 'Allu Sirish',
  ' Vijay Deverakonda', ' Adivi Sesh', 'Satyanarayana Kaikala', 'Sobhan Babu', 'Krishnamraju', 'Uday Kiran', 'Venu Thottempudi', 'Vennela Kishore', 'Gummadi', 'Ali',
- 'Brahmanandam', 'J.D. Chakravarthi') 
+ 'Brahmanandam', 'J.D. Chakravarthi', ' J.V. Somayajulu', 'Sumanth', 'Navdeep', 'Prakash Raj') 
 AND primaryProfession LIKE 'actor%'
 ORDER BY primaryName ASC;
 
+SELECT * FROM imdb.`name.basics` 
+WHERE primaryName IN ('Rajinikanth', 'Kamal Haasan', 'Ajith Kumar', 'Joseph Vijay', 'Suriya', 'Vikram', 'Dhanush', 'Karthi', 'Vishal', 'Jiiva', 'Arya', 'Siddharth', 'Madhavan', 
+'Srikanth', 'Jayam Ravi', 'T.R. Silambarasan', 'Arvind Swamy', 'Arjun Sarja', 'Prabhu', 'Prabhu Deva', 'Vijayakanth', 'Sathyaraj', 'Sarath Kumar', 'Raj Kiran') 
+AND primaryProfession LIKE 'actor%'
+ORDER BY primaryName ASC;
+
+SELECT * FROM imdb.`name.basics` 
+WHERE primaryName IN ('Mohanlal', 'Mammootty', 'Dileep', 'Prithviraj Sukumaran', 'Dulquer Salmaan', 'Suresh Gopi', 'Jayaram') 
+AND primaryProfession LIKE 'actor%'
+ORDER BY primaryName ASC;
+
+SELECT * FROM imdb.`name.basics` 
+WHERE primaryName IN ('Puneeth Rajkumar', 'Sudeep', 'Shivarajkumar', 'Upendra', 'Ambarish', 'Yash') 
+AND primaryProfession LIKE 'actor%'
+ORDER BY primaryName ASC;
+
+SELECT * FROM imdb.`name.basics` 
+WHERE primaryName IN ('Johnny Depp', 'Al Pacino', 'Robert De Niro', 'Kevin Spacey', 'Denzel Washington', 'Russell Crowe', 'Brad Pitt', 'Leonardo DiCaprio', 'Tom Cruise', 
+'John Travolta', 'Arnold Schwarzenegger', 'Sylvester Stallone', 'Christian Bale', 'Morgan Freeman', 'Keanu Reeves', 'Nicolas Cage', 'Hugh Jackman', 'Edward Norton',
+'Bruce Willis', 'Tom Hanks', 'Will Smith', 'Sean Connery', 'Vin Diesel', 'Matt Damon', 'Richard Gere', 'Clive Owen', 'Mel Gibson', 'George Clooney','Jack Nicholson',
+'Tom Hardy', 'Robert Downey Jr.', 'Orlando Bloom', 'Ian McKellen', 'Antonio Banderas', 'Guy Pearce', 'Samuel L. Jackson','Gerard Butler', 'Simon Baker', 'Clint Eastwood') 
+AND primaryProfession LIKE 'actor%'
+ORDER BY primaryName ASC;
+
+SELECT * FROM imdb.`name.basics` 
+WHERE primaryName IN (
+'Dilip Kumar', 'Raj Kapoor', 'Dev Anand', 'Amitabh Bachchan', 'Salman Khan', 'Rajesh Khanna', 'Shah Rukh Khan', 'Aamir Khan', 'Dharmendra', 'Akshay Kumar', 'Ashok Kumar',
+'Sunil Dutt', 'Sanjeev Kumar', 'Naseeruddin Shah', 'Anil Kapoor', 'Jeetendra', 'Sanjay Dutt', 'Rajendra Kumar', 'Vinod Khanna', 'Nana Patekar', 'Shammi Kapoor', 'Guru Dutt', 
+'Sunny Deol', 'Mithun Chakraborty', 'Om Puri', 'Manoj Kumar', 'Pran', 'Prithviraj Kapoor', 'Rishi Kapoor', 'Shatrughan Sinha', 'Saif Ali Khan', 'Hrithik Roshan', 'Feroz Khan',
+'Randhir Kapoor', 'Govinda', 'Jackie Shroff', 'Raaj Kumar', 'Sunil Shetty', 'Balraj Sahni', 'Kishore Kumar', 'Emraan Hashmi', 'Amjad Khan', 'Irrfan Khan', 'Nawazuddin Siddiqui',
+'Ranbir Kapoor', 'Amol Palekar', 'Amrish Puri', 'Anupam Kher', 'Vinod Mehra', 'Sanjay Khan', 'Shahid Kapoor', 'Kay Kay Menon', 'Alok Nath', 'Kader Khan', 'Manoj Bajpayee',
+'Om Prakash', 'Prem Chopra', 'Shakti Kapoor', 'John Abraham', 'Raj Babbar', 'Rakesh Roshan', 'Sanjay Kapoor', 'Bobby Deol', 'Ranveer Singh', 'Varun Dhawan', 'Ranjeet',
+'Johnny Lever', 'Imran Khan', 'Riteish Deshmukh', 'Sidharth Malhotra', 'Arjun Kapoor', 'Abhishek Bachchan', 'Gulshan Grover', 'Fardeen Khan', 'Randeep Hooda', 'Paresh Rawal',
+'Arjun Rampal', 'Akshaye Khanna', 'Pulkit Samrat', 'Sushant Singh Rajput', 'Raza Murad', 'Pankaj Kapur', 'Sanjay Mishra', 'Farooq Shaikh', 'Farhan Akhtar', 'Abhay Deol',
+'Tiger Shroff', 'Arbaaz Khan', 'Vivek Oberoi', 'Aditya Pancholi', 'Deepak Tijori', 'Mohnish Bahl', 'Zayed Khan', 'Annu Kapoor', 'Ayushmann Khurrana', 'Fawad Khan',
+'Arshad Warsi')
+AND primaryProfession LIKE 'actor%'
+ORDER BY primaryName ASC;
