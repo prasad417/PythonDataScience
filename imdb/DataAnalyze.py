aa58353540f6df62d93fd13804c075b3f16a9112
prasad <417.prasad@gmail.com>
2019-07-23 00:05:50 -0500

    SQL query update and pandas

--- imdb/DataAnalyze.py
+++ imdb/DataAnalyze.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import  datetime
+import datetime
 import numpy as np
 
 # nb = pd.read_csv('./csvfiles/name.basics.csv', sep='\t', encoding='utf8')
@@ -33,9 +33,48 @@ import numpy as np
 # movies.to_csv('./csvfiles/movies.csv', sep='\t', encoding='utf-8', index=False)
 
 print(datetime.datetime.now())
+actor_list = [
+'Aadi Pudipeddi', 'Adivi Sesh', 'Akhil Akkineni', 'Akkineni Nageshwara Rao', 'Ali Basha Mohammad', 'Allari Naresh', 'Allu Arjun', 'Allu Sirish',
+    'Brahmanandam', 'Chiranjeevi Konidela', 'Gopichand Tottempudi', 'Gummadi Venkateswara Rao', 'J.D. Chakravarthi', 'J.V. Somayajulu', 'Jagapathi Babu', 'Kalyan Ram',
+    'Krishna Ghattamaneni', 'Krishnamraju', 'Mahesh Babu Ghattamaneni', 'Manoj Kumar Manchu', 'Mohan Babu Manchu', 'N.T. Rama Rao Jr.', 'Naga Chaitanya Akkineni', 'Naga Shaurya',
+    'Nagarjuna Akkineni', 'Nandamuri Balakrishna', 'Nani Ghanta', 'Nara Rohith', 'Navdeep Pallapolu', 'Nikhil Siddharth', 'Nithiin Kumar Reddy',
+    'Pawan Kalyan Konidela', 'Prabhas Raju Uppalapati', 'Prakash Raj', 'Rajendra Prasad Gadde', 'Ram Charan Konidela', 'Ram Pothineni',  'Rana Daggubati', 'Ravi Teja Bhupatiraju', 'S.V. Ranga Rao',
+    'Sai Dharam Tej', 'Sai Srinivas Bellamkonda', 'Satyanarayana Kaikala', 'Sharvanand Myneni', 'Sobhan Babu Uppu', 'Srikanth Meka', 'Sumanth Yarlagadda', 'Sundeep Kishan',
+    'Taraka Rama Rao Nandamuri', 'Uday Kiran', 'Varun Sandesh', 'Varun Tej',  'Venkatesh Daggubati', 'Vennela Kishore Bokkala', 'Venu Thottempudi', 'Vijay Deverakonda',
+'Vishnu Manchu',
+'Rajinikanth Gaekwad', 'Kamal Haasan', 'Ajith Kumar P', 'Joseph Vijay', 'Suriya Sivakumar', 'Vikram Chiyaan', 'Dhanush', 'Karthi Sivakumar',
+    'Vishal Krishna Reddy', 'Jiiva Amar Choudary', 'Arya Jamshad Cethirakath', 'Siddharth Suryanarayan', 'Madhavan Ranganathan','Jayam Ravi', 'T.R. Silambarasan',
+    'Arvind Swamy', 'Arjun Sarja', 'Prabhu Ganesan', 'Prabhu Deva Sundar', 'Vijayakanth', 'Sathyaraj', 'Sarath Kumar Ramanathan', 'Rajkiran Mohideen Abdul Khadar',
+'Lawrence Raghavendra',
+    'Mohanlal', 'Mammootty', 'Dileep', 'Prithviraj Sukumaran', 'Dulquer Salmaan', 'Suresh Gopi', 'Jayaram',
+    'Puneeth Rajkumar', 'Sudeep', 'Shivarajkumar', 'Upendra', 'Ambarish', 'Yash',
+'Dilip Kumar', 'Raj Kapoor', 'Dev Anand', 'Amitabh Bachchan', 'Salman Khan', 'Rajesh Khanna', 'Shah Rukh Khan',
+    'Aamir Khan', 'Dharmendra', 'Akshay Kumar', 'Ashok Kumar',
+    'Sunil Dutt', 'Sanjeev Kumar', 'Naseeruddin Shah', 'Anil Kapoor', 'Jeetendra', 'Sanjay Dutt', 'Rajendra Kumar',
+    'Vinod Khanna', 'Nana Patekar', 'Shammi Kapoor', 'Guru Dutt',
+    'Sunny Deol', 'Mithun Chakraborty', 'Om Puri', 'Manoj Kumar', 'Pran', 'Prithviraj Kapoor', 'Rishi Kapoor',
+    'Shatrughan Sinha', 'Saif Ali Khan', 'Hrithik Roshan', 'Feroz Khan',
+    'Randhir Kapoor', 'Govinda', 'Jackie Shroff', 'Raaj Kumar', 'Sunil Shetty', 'Balraj Sahni', 'Kishore Kumar',
+    'Emraan Hashmi', 'Amjad Khan', 'Irrfan Khan', 'Nawazuddin Siddiqui',
+    'Ranbir Kapoor', 'Amol Palekar', 'Amrish Puri', 'Anupam Kher', 'Vinod Mehra', 'Sanjay Khan', 'Shahid Kapoor',
+    'Kay Kay Menon', 'Alok Nath', 'Kader Khan', 'Manoj Bajpayee',
+    'Om Prakash', 'Prem Chopra', 'Shakti Kapoor', 'John Abraham', 'Raj Babbar', 'Rakesh Roshan', 'Sanjay Kapoor',
+    'Bobby Deol', 'Ranveer Singh', 'Varun Dhawan', 'Ranjeet',
+    'Johnny Lever', 'Imran Khan', 'Riteish Deshmukh', 'Sidharth Malhotra', 'Arjun Kapoor', 'Abhishek Bachchan',
+    'Gulshan Grover', 'Fardeen Khan', 'Randeep Hooda', 'Paresh Rawal',
+    'Arjun Rampal', 'Akshaye Khanna', 'Pulkit Samrat', 'Sushant Singh Rajput', 'Raza Murad', 'Pankaj Kapur',
+    'Sanjay Mishra', 'Farooq Shaikh', 'Farhan Akhtar', 'Abhay Deol',
+    'Tiger Shroff', 'Arbaaz Khan', 'Vivek Oberoi', 'Aditya Pancholi', 'Deepak Tijori', 'Mohnish Bahl', 'Zayed Khan',
+    'Annu Kapoor', 'Ayushmann Khurrana', 'Fawad Khan',
+'Arshad Warsi',
+    'Johnny Depp', 'Al Pacino', 'Robert De Niro', 'Kevin Spacey', 'Denzel Washington', 'Russell Crowe', 'Brad Pitt', 'Leonardo DiCaprio', 'Tom Cruise',
+'John Travolta', 'Arnold Schwarzenegger', 'Sylvester Stallone', 'Christian Bale', 'Morgan Freeman', 'Keanu Reeves', 'Nicolas Cage', 'Hugh Jackman', 'Edward Norton',
+'Bruce Willis', 'Tom Hanks', 'Will Smith', 'Sean Connery', 'Vin Diesel', 'Matt Damon', 'Richard Gere', 'Clive Owen', 'Mel Gibson', 'George Clooney','Jack Nicholson',
+'Tom Hardy', 'Robert Downey Jr.', 'Orlando Bloom', 'Ian McKellen', 'Antonio Banderas', 'Guy Pearce', 'Samuel L. Jackson','Gerard Butler', 'Simon Baker', 'Clint Eastwood'
+]
 mv = pd.read_csv('./csvfiles/movies.csv', sep='\t', encoding='utf8')
 # v = mv[mv.primaryName.isin(['Venkatesh Daggubati'])]
-v = mv[np.isin(mv['primaryName'], ['Venkatesh Daggubati'])]
+v = mv[np.isin(mv['primaryName'], actor_list)]
 print(datetime.datetime.now())
 print(v)
 # Select only 2 columns from dataFrame and create a new subset DataFrame
